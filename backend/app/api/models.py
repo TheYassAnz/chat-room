3efc960e5eaefc1a05110bad3a7d21c3905df073
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-05-02 23:07:00 +0200

    feat: auto generate code attribute for Room Model

--- backend/app/api/models.py
+++ backend/app/api/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
+import string
+import random
+all_characters = string.digits + string.ascii_letters
+generatedCode = ''.join(random.choices(all_characters, k=7))
+
 # Create your models here.
 class Room(models.Model):
-    code = models.CharField(max_length=50, null=False, unique=True, primary_key=True)
+    code = models.CharField(max_length=50, null=False, unique=True, primary_key=True, default=generatedCode.upper(), auto_created=True, editable=False)
     name = models.CharField(max_length=50, null=False)
     creation_date = models.DateTimeField(auto_now_add=True)
     
