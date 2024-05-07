5a19d3802f740316880932c2edbacc2a447b99cb
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-05-07 16:28:27 +0200

    feat: includes room informations

--- frontend/src/pages/Room/index.jsx
+++ frontend/src/pages/Room/index.jsx
@@ -1,11 +1,24 @@
 import { useParams } from "react-router-dom"
+import axios from "axios";
+import { useEffect, useState } from "react";
 export default function Room() {
-    // Get the code parameter from the URL
     const { code } = useParams()
+    const [data, setData] = useState({})
+
+    useEffect(() => {
+        const fetchData = async () => {
+            await axios.get(`http://localhost:8000/api/rooms/${code}`)
+                .then((response) => {
+                    setData(response.data)
+                })
+                .catch((error) => console.error(error))
+        }
+        fetchData()
+    }, [])
     return (
         <>
             <div className="xl:mx-96 md:mx-56 sm:mx-32 mx-8 mt-12">
-                <h1 className="text-2xl font-semibold text-gray-900 text-center">Room {code}</h1>
+                <h1 className="text-2xl text-gray-900 text-center">Welcome to the room <span className="font-bold">{data.code}</span> !</h1>
             </div>
         </>
     )
