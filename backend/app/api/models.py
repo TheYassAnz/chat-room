ca651ab00824903ef255c688af3721bd50b5c428
Yassine ANZAR BASHA <yassanz.contact@gmail.com>
2024-05-07 15:52:18 +0200

    feat: generate the room's code auto

--- backend/app/api/models.py
+++ backend/app/api/models.py
@@ -1,15 +1,21 @@
 from django.db import models
 
-import string
-import random
-all_characters = string.digits + string.ascii_letters
-generatedCode = ''.join(random.choices(all_characters, k=7))
+import uuid
+
 
 # Create your models here.
+def generate_unique_code():
+    return str(uuid.uuid4()).upper()[:8]  # Génère un code unique de 8 caractères
+
 class Room(models.Model):
-    code = models.CharField(max_length=50, null=False, unique=True, primary_key=True, default=generatedCode.upper(), auto_created=True, editable=False)
+    code = models.CharField(max_length=50, null=False, primary_key=True, unique=True, editable=False)
     name = models.CharField(max_length=50, null=False)
     creation_date = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.code:  # Vérifie si le code est déjà défini
+            self.code = generate_unique_code()
+        super(Room, self).save(*args, **kwargs)
     
 class Post(models.Model):
     text = models.TextField()
